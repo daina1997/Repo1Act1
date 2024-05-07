8f9b379ad255b5246de8cd1816ef57d9253e2cb2
Daina DB <daina.97.db@gmail.com>
2024-05-07 13:16:28 +0200

    manager ha creado proyecto base

---
+++ src/dao/TuNombreDaoImpl.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class TuNombreDaoImpl implements TuNombreDao {
+
+	@Override
+	public String mellamo(String Nombre, String Apellidos) {
+		
+		return "Me llamo: " + Nombre + " " + Apellidos;
+	}
+}
---
+++ src/testing/TestNombre.java
@@ -0,0 +1,18 @@
+package testing;
+
+import dao.TuNombreDaoImpl;
+
+public class TestNombre {
+
+	private static TuNombreDaoImpl testNombre;
+	static {
+		testNombre= new TuNombreDaoImpl();
+	}
+	public static void main(String[] args) {
+		
+		mellamo();
+	}
+	public static void mellamo() {
+		System.out.println(testNombre.mellamo("Maria", "Barceló"));
+	}
+}
